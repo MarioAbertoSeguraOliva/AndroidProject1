916497dbb7ee2d32298cc8f50e6373b281f7b2e5
Xirzag <albertoesbueno@hotmail.com>
2015-12-27 22:51:54 +0000

    Athlete_can_send_message_to_group test passed

--- app/src/test/java/com/github/androidproject/users/Athlete_.java
+++ app/src/test/java/com/github/androidproject/users/Athlete_.java
@@ -33,4 +33,40 @@ public class Athlete_ {
         assertThat(group.users().contains(coach), is(true));
     }
 
+    @Test
+    public void can_send_message_to_group() throws Exception {
+        Coach coach = new Coach("Juan Carlos");
+        Group group = coach.createGroup("Buenas tardes tardes");
+        Athlete athlete = new Athlete("Jose");
+        Athlete athlete2 = new Athlete("Mario");
+        Athlete athlete3 = new Athlete("Alberto");
+
+        coach.addUser(athlete).to(group);
+        coach.addUser(athlete2).to(group);
+        coach.addUser(athlete3).to(group);
+        assertThat(group.users().size(), is(4));
+
+        athlete.sendMessage("No quiero estudiar redes").to(group.chat());
+        assertThat(group.chat().messages().size(), is(1));
+        assertThat(group.chat().messages().get(0).sender(), CoreMatchers.<User>is(athlete));
+        assertThat(group.chat().messages().get(0).content(), is("No quiero estudiar redes"));
+
+        athlete2.sendMessage("Estamos haciendo una practica que es imposible").to(group.chat());
+        assertThat(group.chat().messages().size(), is(2));
+        assertThat(group.chat().messages().get(1).sender(), CoreMatchers.<User>is(athlete2));
+        assertThat(group.chat().messages().get(1).content(), is("Estamos haciendo una practica que es imposible"));
+
+        athlete.sendMessage("Pues me voy a comer").to(group.chat());
+        assertThat(group.chat().messages().size(), is(3));
+        athlete3.sendMessage("Espera!!").to(group.chat());
+        assertThat(group.chat().messages().size(), is(4));
+
+        assertThat(group.chat().messages().get(2).sender(), CoreMatchers.<User>is(athlete));
+        assertThat(group.chat().messages().get(2).content(), is("Pues me voy a comer"));
+        assertThat(group.chat().messages().get(3).sender(), CoreMatchers.<User>is(athlete3));
+        assertThat(group.chat().messages().get(3).content(), is("Espera!!"));
+
+        assertThat(group.users().size(), is(4));
+    }
+
 }
